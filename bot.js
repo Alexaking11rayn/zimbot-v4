3760942be1dc0ad21585e16034cfbd58fbeb44ef
Alexaking11rayn <108949984+Alexaking11rayn@users.noreply.github.com>
2022-08-18 12:42:12 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
+global.owner = ['923189718115',''] //owner number +923189718115
+global.wame = ['923189718115'] 
+global.pemilik = ['923189718115'] //owner number +923189718115
+global.premium = ['923189718115'] //premium number +923189718115
+global.pengguna = 'RAYAN' //username
+global.footer = 'RAYAN BOT V1'
+global.botnma = 'RAYAN BOT V1' //bot name
 global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.ownernma = 'RAYAN KHAN' //owner name
+global.packname = 'RAYAN KHAN ' //sticker package name
+global.author =   'by rathless boy'//sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
@@ -55,7 +55,7 @@ global.available = false
 global.unavailable = true
 global.limittot = 100,
 global.thumb = fs.readFileSync('./Zimbot/drips.jpg')
-global.reactmoji = '🇿🇼'
+global.reactmoji = '🇵🇰'
 global.imgjoinrpg = fs.readFileSync('./image/joinrpg.jpg')
 global.imgmining = fs.readFileSync('./image/mining.jpg')
 global.imgdevil = fs.readFileSync('./image/img/devil.jpg')
